76641695188d026952d46f2db6cce2cb208e9c39
Ruben Franco <ruben.afranco@yahoo.com>
2023-04-26 19:12:16 -0700

    added create user and update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,5 +22,28 @@ module.exports = {
           .catch((err) => res.status(500).json(err));
     },
 
-    
+    //create a user
+    createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+    },
+
+    //update a user
+    updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User find with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+    },
 };
