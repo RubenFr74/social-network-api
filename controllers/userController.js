6927bef403484109a2382598d547a45e456649a0
Ruben Franco <ruben.afranco@yahoo.com>
2023-04-26 19:09:42 -0700

    set up userController and added get all users

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  //Get all users
+  getUser(req, res) {
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+};
