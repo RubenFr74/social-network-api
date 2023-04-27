403fc4f8391eb861a461e9b03479c889f0c7da24
Ruben Franco <ruben.afranco@yahoo.com>
2023-04-26 19:05:34 -0700

    added delete thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,6 +7,7 @@ module.exports = {
         .then((thought) => res.json(thought))
         .catch((err) => res.status(500).json(err));
     },
+
     // gets a single thought
     getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
@@ -17,7 +18,8 @@ module.exports = {
               : res.json(thought)
           )
           .catch((err) => res.status(500).json(err));
-      },
+    },
+
     //create a thought and push the created thought's _id to the associated user's thoughts array field
     createThought(req, res) {
     Thought.create(req.body)
@@ -34,7 +36,7 @@ module.exports = {
           : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
-  },
+    },
 
     //update a thought
     updateThought(req, res) {
@@ -49,37 +51,28 @@ module.exports = {
               : res.json(user)
           )
           .catch((err) => res.status(500).json(err));
-      },
+    },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //delete a thought
+    deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought find with this ID!" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'Thought deleted, but no user found'})
+          : res.json({ message: 'Thought successfully deleted' })
+      )
+      .catch((err) => res.status(500).json(err));
+    },
+    
 
 
 
