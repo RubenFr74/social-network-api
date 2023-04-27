d3bc5ec2de46845011db2ddfcd92819459b171f6
Ruben Franco <ruben.afranco@yahoo.com>
2023-04-26 19:02:25 -0700

    added update thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -36,6 +36,21 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
+    //update a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $set: req.body },
+          { runValidators: true, New: true }
+        )
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: "No thought find with this ID!" })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+
 
 
 
