43e53b641f77b7d31a9b58b46360e99cd568d806
Ruben Franco <ruben.afranco@yahoo.com>
2023-04-27 21:04:29 -0700

    small change to the match email

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const userSchema = new Schema(
       required: true,
       unique: true,
       match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        /.+@.+\..+/,
         "Please fill a valid email address",
       ],
     },
